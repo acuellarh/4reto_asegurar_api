1bdabcfb03c26fdc57a292e2cfc139e4989f5e2c
Alejandro <acuellarh@gmail.com>
2020-03-14 09:51:30 -0500

    Application controller adjusted to 401 message respond

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def authenticate
-    email = request.headers['X-User-Email']
-    api_token = request.headers['X-Api-Token']
-    @user = User.find_by(email: email, api_token: api_token)
-
-    unless @user
-      head status: :unauthorized
-      return false
-    end
+    email_auth = request.headers["X-User-Email"]
+    token_auth = request.headers["X-Api-Token"]
+    head 401 unless User.find_by_email(email_auth) && User.find_by_api_token(token_auth)
   end
 
 end
